9eb5562e28b99f8971ca576a80e5eaf541df6730
AngelMoralesK <angelmorales@deloittemx.com>
2018-11-19 08:56:59 -0600

    Initial Commit

---
+++ CoreDataDealer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDealer
+//
+//  Created by Morales, Angel (MX - Mexico) on 19/11/18.
+//  Copyright © 2018 TheKairuz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
