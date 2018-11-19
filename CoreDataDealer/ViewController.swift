5f653fe7aa0d0cb63964adb062030ecf6604b3f7
AngelMoralesK <a.jesse3001@gmail.com>
2018-11-19 15:56:16 -0600

    creating function updateUI, implement function for the create and deleteRecords

--- CoreDataDealer/ViewController.swift
+++ CoreDataDealer/ViewController.swift
@@ -10,22 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let manager = CoreDataManager()
+    var counter = 0
     @IBOutlet weak var summaryLabel: UILabel! {
         didSet {
-            summaryLabel.text = "Intento: \(0)\r\nRegistros en la base: \(0)\r\nÚltimo registro: nil"
+            summaryLabel.text = "Intento: \(counter)\r\nRegistros en la base: \(0)\r\nÚltimo registro: nil"
+        }
+    }
+
+    
+    @IBAction func createRecords(_ sender: UIButton) {
+        manager.createUser(name: "Juan", lastName: "Perez", email: "juanperez@mail.com", initialAmount: 2000.50) { [weak self] in
+            self?.updateUI()
         }
     }
     
-    
-    @IBAction func createRecords(_ sender: UIButton) {
-    }
-    
     @IBAction func deleteRecords(_ sender: UIButton) {
+        manager.deleteUsers()
+        counter = -1
+        updateUI()
     }
     
     @IBAction func fetchLastRecord(_ sender: UIButton) {
+        
     }
 
-
+    func updateUI() {
+        counter = counter + 1
+        let users = manager.fetchUsers()
+        summaryLabel.text = "Intento: \(counter) \r\nRegistros en la base: \(users.count) \r\nÚltimo registro: \(users.last?.name)"
+        
+    }
 }
 
